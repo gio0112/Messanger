f7e56d53f2fdc32b5f101495b1df5fdc107573a6
giorgi gogiashvili <gogiashvili12@outlook.com>
2020-04-18 14:35:24 +0400

    Add Person model

---
+++ DataLayer/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLayer.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public byte Status { get; set; }
+    }
+}
